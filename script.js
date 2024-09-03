19bb1dec345e0d7ae5865101369b207371d462f7
Tracey Philips <79120313+Phiacc@users.noreply.github.com>
2024-09-03 15:24:19 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+    // This JavaScript is used to trigger the loading animation.
+    window.onload = function(){
+        document.querySelector('.love-bar-container').classList.add('loading');
+    }
+    
